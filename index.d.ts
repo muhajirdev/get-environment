a0fa41876d28124016875a538275fd304aa3bc7a
Muhammad Muhajir <muhajirframe@gmail.com>
2019-05-19 07:55:30 +0700

    chore: generate ts typing

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export declare const getEnvAsArray: (env: string) => string[];
+export declare const getEnvAndEnsure: (envProp: string) => string | boolean;
+export declare const getManyEnvAndEnsure: (envProps: string[]) => (string | boolean)[];
